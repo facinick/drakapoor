f101bdccc8ded0600ba4e0e388de7e0f97d0518e
facinick <facinick@gmail.com>
2024-06-27 19:21:22 +0530

    removed unncess data

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
     <Item interactionAnimation={false} height={4}><ReviewsCarousal /></Item>
     <Item height={1}><PhoneWidget analyticsProps={{ eventName: "phone_click" }} url={'tel:+919820381015'} text={"Appointment on Phone"} size={1} /></Item>
     <Item height={2}><ExternalLinkWidget analyticsProps={{ eventName: "location_click" }} cover={medicare} size={2} url={'https://goo.gl/maps/ruWqJkW9XzQSkHtcA'} text={'Lifeline Medicare Hospital'} brand={'googlebusiness'} /></Item>
-    <Item height={1}><ExternalLinkWidget analyticsProps={{ eventName: "botim_click" }} size={1} text={'+91 703,964,7409 Botim (UAE)'} brand={'botim'} url={''} cover={undefined} /></Item>
+    {/* <Item height={1}><ExternalLinkWidget analyticsProps={{ eventName: "botim_click" }} size={1} text={'+91 703,964,7409 Botim (UAE)'} brand={'botim'} url={''} cover={undefined} /></Item> */}
     <Item height={1}><WhatsappWidget analyticsProps={{ eventName: "whatsapp_catalog_click" }} url={'https://wa.me/c/919820381015'} text='Offered Services on WhatsApp' size={1} /></Item>
     <Section width={sectionWidth}><TextWidget tag='h2' text='References' size={1} /></Section>
     <Item height={2}><ExternalLinkWidget analyticsProps={{ eventName: "practo_click" }} cover={practo} size={2} url={'https://www.practo.com/mumbai/clinic/dr-anuradha-kapoor-s-diabetes-care-clinic-malad-2'} text={'Dr Anuradha Kapoor, Practo'} brand={'practo'} /></Item>
@@ -51,7 +51,7 @@ export default function Home() {
     <Item height={2}><ExternalLinkWidget cover={youtube2} size={2} url={'https://www.youtube.com/watch?v=L0IzTiAa4oc'} text={'Dr. Anuradha Kapoor (Strong Bones)'} brand={'youtube'} /></Item>
     <Item height={2}><ExternalLinkWidget cover={youtube3} size={2} url={'https://www.youtube.com/watch?v=jKG2YxQKRhA&t=30s'} text={'What happens if your Sugar level is not under control?'} brand={'youtube'} /></Item>
     <Item height={2}><ExternalLinkWidget cover={youtube2} size={2} url={'https://www.youtube.com/watch?v=o4sp_BqsXks'} text={'Vitamin D3 for bones'} brand={'youtube'} /></Item> */}
-    <Section width={sectionWidth}><TextWidget tag='h2' text='Diabetes Resources' size={1} /></Section>
+    {/* <Section width={sectionWidth}><TextWidget tag='h2' text='Diabetes Resources' size={1} /></Section>
     <Item height={1}><ExternalLinkWidget cover={''} size={1} url={'https://www.facebook.com/diabetesdaily/'} text={'Diabetes Daily'} brand={'facebook'} /></Item>
     <Item height={1}><ExternalLinkWidget cover={''} size={1} url={'https://www.facebook.com/diabetesuk/'} text={'Diabetes UK'} brand={'facebook'} /></Item>
     <Section width={sectionWidth}><TextWidget tag='h2' text='Special thanks 🙏🏻' size={1} /></Section>
@@ -73,7 +73,7 @@ export default function Home() {
     <Item height={1}><ExternalLinkWidget cover={''} size={1} url={'https://www.icicilombard.com/health-insurance/health-advantedge-insurance-for-family?utm_source=google_search&utm_medium=CPC&utm_campaign=Health_Brand_Core_HI_Core_Exact_18.11.2022&utm_adgroup=Core&utm_keyword=icici%20health%20insurance&utm_device=c&utm_ad=657155057160&utm_placement=&utm_network=g&utm_matchtype=e&gad=1&gclid=Cj0KCQjwj_ajBhCqARIsAA37s0wxPjCD_fAY0OUZt_96XTMa2gG6aCBSaIQyZ3b17ZbOqyNULyf5aGgaApHcEALw_wcB'} text={'ICICI Lombard Health Insurance'} brand={'icici'} /></Item>
     <Section width={sectionWidth}><TextWidget tag='h2' text='Bankers' size={1} /></Section>
     <Item height={1}><ExternalLinkWidget cover={''} size={1} url={'https://www.icicibank.com/'} text={'ICICI Netbanking Services'} brand={'icici'} /></Item>
-    <Item height={1}><ExternalLinkWidget cover={''} size={1} url={'https://www.onlinesbi.sbi/'} text={'State Bank of India'} brand={'sbi'} /></Item>
+    <Item height={1}><ExternalLinkWidget cover={''} size={1} url={'https://www.onlinesbi.sbi/'} text={'State Bank of India'} brand={'sbi'} /></Item> */}
   </>)
 
   useEffect(() => {
