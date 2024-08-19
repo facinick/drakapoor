b973b31ca90739768cd3fcac23b1e1606794ff22
facinick <facinick@gmail.com>
2024-08-20 02:43:46 +0530

    removed unused files

--- components/layout/nav.tsx
+++
@@ -1,9 +0,0 @@
-// import Navbar from "./navbar";
-// import { getServerSession } from "next-auth/next";
-// import { GET } from "@/app/api/auth/[...nextauth]/route";
-
-// export default async function Nav() {
-//   const session = await getServerSession(GET());
-//   return <Navbar session={session} />;
-// }
-export default {}
